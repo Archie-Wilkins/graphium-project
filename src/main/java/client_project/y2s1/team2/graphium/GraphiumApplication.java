9d159b4c0cb555d45c42185585de4c5cc229e06a
Samuel Toland <TolandSO@Cardiff.ac.uk>
2021-11-15 12:55:54 +0000

    Initial Spring Boot upload
    Added intial files for Spring Boot including:
    	Spring Web
    	Spring Dev tools
    	Spring Security
    	Spring JPA
    	Thymleaf
    	Lombok

---
+++ src/main/java/client_project/y2s1/team2/graphium/GraphiumApplication.java
@@ -0,0 +1,13 @@
+package client_project.y2s1.team2.graphium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphiumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/client_project/y2s1/team2/graphium/GraphiumApplicationTests.java
@@ -0,0 +1,13 @@
+package client_project.y2s1.team2.graphium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
