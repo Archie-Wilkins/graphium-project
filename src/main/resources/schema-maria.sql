b5329b24fed52fca568b1cec7def30d036becced
Archie Wilkins <wilkinsat1@cardiff.ac.uk>
2021-11-25 17:17:57 +0000

    Pairprogrammed with Sam added essential parts of new db schema

--- src/main/resources/schema-maria.sql
+++ src/main/resources/schema-maria.sql
@@ -8,6 +8,7 @@ use `graphium`;
 drop table if exists `users`;
 drop table if exists `authorities`;
 drop table if exists `documents`;
+drop table if exists `organisations`;
 
 -- Code based on from https://www.baeldung.com/spring-security-jdbc-authentication tutorial
 
@@ -15,11 +16,19 @@ drop table if exists `documents`;
 -- Table 'users'
 -- ------------------------------------
 CREATE TABLE IF NOT EXISTS `users` (
-    `username` VARCHAR(50) NOT NULL PRIMARY KEY,
+    `user_ID` INT NOT NULL PRIMARY KEY,
+    `username` VARCHAR(50),
     `password` VARCHAR(500) NOT NULL,
-    `enabled` BOOLEAN NOT NULL
+    `enabled` BOOLEAN NOT NULL,
+    `organisation_id` INT NOT NULL,
+    `first_name` VARCHAR(50) NOT NULL,
+    `last_name` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `authority_title` VARCHAR(50) NOT NULL, 
+    `authority_set_date` DATETIME NOT NULL,
+    
+     FOREIGN KEY (`organisation_id`) REFERENCES organisations(`id`)
 );
-
 
 -- ------------------------------------
 -- Table 'authorities'
@@ -27,28 +36,35 @@ CREATE TABLE IF NOT EXISTS `users` (
 CREATE TABLE IF NOT EXISTS `authorities` (
     `username` VARCHAR(50) NOT NULL,
     `authority` VARCHAR(50) NOT NULL,
-    CONSTRAINT `fk_authorities_users` FOREIGN KEY (`username`) REFERENCES users(`username`)
-);
 
+    FOREIGN KEY (`username`) REFERENCES users(`username`)
+);
 
 -- ------------------------------------
 -- Table `documents`
 -- ------------------------------------
 CREATE TABLE IF NOT EXISTS `documents` (
     `id` INT(4) NOT NULL AUTO_INCREMENT,
-    `fk_username` VARCHAR(50) NOT NULL,
+    `fk_creator` VARCHAR(50) NOT NULL,
     `title` VARCHAR(100) NOT NULL,
     `file_type` VARCHAR(20) NOT NULL,
-    `visibility` VARCHAR(45) NOT NULL,
     `file_data` LONGBLOB NOT NULL,
 
     PRIMARY KEY(`id`),
-    FOREIGN KEY (`fk_username`) REFERENCES users(`username`)
+    FOREIGN KEY (`fk_creator`) REFERENCES users(`username`)
 );
-
 
 -- ------------------------------------
 -- Unique Usernames - ensures that all values in a coloumn are different
 -- ------------------------------------
 CREATE UNIQUE INDEX `ix_auth_username`
     on authorities(`username`,`authority`);
+
+-- ------------------------------------
+-- organisations
+-- -----------------------------------
+CREATE TABLE IF NOT EXISTS `organisations`(
+    `id` INT AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100)
+);
