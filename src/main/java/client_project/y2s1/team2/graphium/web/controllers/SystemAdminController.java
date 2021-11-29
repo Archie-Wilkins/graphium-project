33f5117535968b7c642d2bca2f3ba5150460afad
Archie Wilkins <wilkinsat1@cardiff.ac.uk>
2021-11-29 22:31:12 +0000

    Started adding form - added organisations jpa

--- src/main/java/client_project/y2s1/team2/graphium/web/controllers/SystemAdminController.java
+++ src/main/java/client_project/y2s1/team2/graphium/web/controllers/SystemAdminController.java
@@ -2,6 +2,7 @@ package client_project.y2s1.team2.graphium.web.controllers;
 
 import client_project.y2s1.team2.graphium.data.jpa.entities.Documents;
 import client_project.y2s1.team2.graphium.data.jpa.repositories.DocumentsRepositoryJPA;
+import client_project.y2s1.team2.graphium.domain.SubmissionError;
 import client_project.y2s1.team2.graphium.service.StoreFileDatabaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -9,6 +10,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.security.Principal;
 import java.util.List;
@@ -32,11 +36,27 @@ public class SystemAdminController {
             return "systemAdminHome.html";
 
     }
-//
-//    @GetMapping({"/systemAdmin/organisation", "systemAdmin"})
-//    public String systemAdminOrganisation(Model model, Principal principal){
-//        return "systemAdminNewOrganisation.html";
-//    }
+
+    @GetMapping({"/systemAdmin/organisation"})
+    public String systemAdminOrganisation(
+            Model model,
+            Principal principal){
+        return "systemAdminNewOrganisation.html";
+    }
+
+    @PostMapping({"/systemAdmin/organisation"})
+    public String systemAdminOrganisation(
+    @RequestParam("organisationNameInput") String receivedName,
+    @RequestParam("emailInput") String recievedEmail,
+    Model model,
+    Principal principal){
+
+        try{
+
+        }
+        return "systemAdminNewOrganisation.html";
+
+    }
 
 
 
