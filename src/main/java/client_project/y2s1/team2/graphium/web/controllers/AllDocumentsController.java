c472f18f6375efd38f9f63326524f345b117fd29
Ahmad Shahin <Shahina@cardiff.ac.uk>
2021-12-01 02:21:24 +0000

    Added "Download" button functionality

---
+++ src/main/java/client_project/y2s1/team2/graphium/web/controllers/AllDocumentsController.java
@@ -0,0 +1,24 @@
+package client_project.y2s1.team2.graphium.web.controllers;
+
+import client_project.y2s1.team2.graphium.data.jpa.entities.Documents;
+import client_project.y2s1.team2.graphium.data.jpa.repositories.DocumentsRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class AllDocumentsController {
+
+    @Autowired
+    private DocumentsRepositoryJPA docsRepo;
+
+    @GetMapping ({"/download", "download"})
+    public String index(Model model){
+        List<Documents> AllDocs = docsRepo.findAll();
+        model.addAttribute("allDocs", AllDocs);
+        return "allDocuments.html";
+    }
+}
