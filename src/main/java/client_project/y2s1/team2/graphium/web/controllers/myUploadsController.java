579fb5ea2752b9ca53edec7811a8061a6f8254f3
Martin Manasiev <manasievm@cardiff.ac.uk>
2021-12-02 09:12:12 +0000

    Implementation of search bar for the documents page - working

--- src/main/java/client_project/y2s1/team2/graphium/web/controllers/myUploadsController.java
+++ src/main/java/client_project/y2s1/team2/graphium/web/controllers/myUploadsController.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import java.util.List;
+
 
 @Controller
 public class myUploadsController {
@@ -16,9 +16,15 @@ public class myUploadsController {
     private DocumentsRepositoryJPA docsRepo;
 
     @GetMapping({"/myuploads", "myuploads"})
-    public String myuploads(Model model){
+    public String myuploads(Model model, String keyword){
         List<Documents> AllDocuments = docsRepo.findAll();
         model.addAttribute("allDocuments", AllDocuments);
+
+        if(keyword !=null){
+            model.addAttribute("allDocuments", docsRepo.findByTitle(keyword));
+        }else{
+            model.addAttribute("allDocuments", docsRepo.findAll());
+        }
         return "myuploads.html";
     }
 }
