b299ea1ce97978784396da228538479659781005
Archie Wilkins <wilkinsat1@cardiff.ac.uk>
2021-12-12 22:51:04 +0000

    For Sam #Welovetests #We'llSleepWhenWe'reDead

--- Team Resources/SeleniumTests/userCanUploadFileTest.py
+++ Team Resources/SeleniumTests/userCanUploadFileTest.py
@@ -2,14 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-
 driver = webdriver.Chrome('./chromedriver')
+driver.fullscreen_window()
+
+def byPassConnectionNotPrivate():
+    driver.get("https://localhost:8443/")
+
+    advanceSafetyOptions = driver.find_element(By.XPATH,'//*[@id="details-button"]');
+    advanceSafetyOptions.click()
+
+    proceedButton = driver.find_element(By.XPATH,'//*[@id="proceed-link"]');
+    proceedButton.click()
+
 
 
 def startUp():
-    driver.get("http://localhost:8080/")
+    driver.get("https://localhost:8443/")
     print(driver.title)
     assert "Graphium" in driver.title
+    driver.fullscreen_window()
+
 
 def logInAsSystemAdmin():
     usernameField = driver.find_element(By.XPATH,"/html/body/form/div[2]/input");
@@ -25,12 +37,26 @@ def logInAsSystemAdmin():
     print(driver.title)
 
 def goToSystemAdminPageUsingNavBar():
-    systemAdminButton = driver.find_element(By.XPATH,"/html/body/nav/nav/div/div[2]/ul/li[1]/a");
+    systemAdminButton = driver.find_element(By.XPATH,"//*[@id="navbarSupportedContent"]/ul/li[4]/a");
     systemAdminButton.click()
     assert "System Admin" in driver.title
     print(driver.title)
 
-# Process
+def goToSystemAdminPageUsingNavBar():
+    systemAdminNewOrgButton = driver.find_element(By.XPATH,"//*[@id="navbarSupportedContent"]/ul/li[4]/a");
+    systemAdminNewOrgButton.click()
+    assert "New Organisation" in driver.title
+    print(driver.title)
+
+def checkSystemAdminFiles():
+    print("Complete this")
+
+def createNewOrganisation():
+    print("Complete this")
+
+
+# Can Create New Org
+byPassConnectionNotPrivate()
 startUp()
 logInAsSystemAdmin()
 goToSystemAdminPageUsingNavBar()
