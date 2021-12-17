d2413e4604fe5592f02e9f9815d33198e8cd9e04
Archie Wilkins <wilkinsat1@cardiff.ac.uk>
2021-12-17 14:47:56 +0000

    Made authentication handlers protected

--- src/main/java/client_project/y2s1/team2/graphium/Security/userAuthenticationSuccessHandler.java
+++ src/main/java/client_project/y2s1/team2/graphium/Security/userAuthenticationSuccessHandler.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class userAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+class userAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
     @Autowired
     AuditService auditService;
