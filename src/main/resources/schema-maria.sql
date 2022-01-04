d874f7aa38fc9ce0a65d8287516edb94794782a6
Archie Wilkins <wilkinsat1@cardiff.ac.uk>
2022-01-04 15:40:52 +0000

    Added documents auditing (Not used in Java Assessment

--- src/main/resources/schema-maria.sql
+++ src/main/resources/schema-maria.sql
@@ -111,7 +111,8 @@ CREATE TABLE IF NOT EXISTS `documents` (
     `title` VARCHAR(100) NOT NULL,
     `date` VARCHAR(20) NOT NULL,
     `file_type` VARCHAR(20) NOT NULL,
-    -- need to make Not NUll just for testing
+    -- need to make Not NUll for testing as creating instances 
+    -- with valid blob data from mySQL requires hard-coding paths
     `file_data` LONGBLOB,
 
     CONSTRAINT `fk_documents_users` FOREIGN KEY (`fk_creator`) REFERENCES users(`username`)
@@ -205,12 +206,11 @@ CREATE UNIQUE INDEX `ix_auth_username`
 -- Sample Improved Auditing System 
 -- ----------------------------------
 DROP TABLE IF EXISTS `users_audit`;
-
 CREATE TABLE IF NOT EXISTS `users_audit` (
     `audit_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `timestamp` DATETIME NOT NULL ,
     `type` VARCHAR(20) NOT NULL,
-    `creator` VARCHAR(250) NOT NULL,
+    `db_user` VARCHAR(250) NOT NULL,
     `username` VARCHAR(50) NOT NULL, 
     `password` VARCHAR(100) NOT NULL, 
     `enabled` BOOLEAN NOT NULL, 
@@ -228,12 +228,12 @@ CREATE TRIGGER users_insert_audit_trigger
 AFTER INSERT ON users FOR EACH ROW 
 BEGIN
     INSERT INTO users_audit (
-        audit_id, timestamp, type, creator, username, password, enabled, fk_organisation_id, 
+        audit_id, timestamp, type, db_user, username, password, enabled, fk_organisation_id, 
         first_name, last_name, email, authority_set_date
 	)
     VALUES(
-		null, CURRENT_TIMESTAMP, 'INSERT', user(), new.username, new.password,
-		new.enabled, new.fk_organisation_id, new.first_name, new.last_name, new.email ,new.authority_set_date
+		null, CURRENT_TIMESTAMP, 'INSERT', user(), NEW.username, NEW.password,
+		NEW.enabled, NEW.fk_organisation_id, NEW.first_name, NEW.last_name, NEW.email, NEW.authority_set_date
 	);
 END $$ 
 DELIMITER ;
@@ -244,12 +244,12 @@ CREATE TRIGGER users_update_audit_trigger
 AFTER UPDATE ON users FOR EACH ROW 
 BEGIN
     INSERT INTO users_audit (
-        audit_id, timestamp, type, creator, username, password, enabled, fk_organisation_id, 
+        audit_id, timestamp, type, db_user, username, password, enabled, fk_organisation_id, 
         first_name, last_name, email, authority_set_date
 	)
     VALUES(
-		null, CURRENT_TIMESTAMP, 'UPDATE', user(), new.username, new.password,
-		new.enabled, new.fk_organisation_id, new.first_name, new.last_name, new.email ,new.authority_set_date
+		null, CURRENT_TIMESTAMP, 'UPDATE', user(), NEW.username, NEW.password,
+		NEW.enabled, NEW.fk_organisation_id, NEW.first_name, NEW.last_name, NEW.email ,NEW.authority_set_date
 	);
 END $$ 
 DELIMITER ;
@@ -260,7 +260,7 @@ CREATE TRIGGER users_delete_audit_trigger
 AFTER DELETE ON users FOR EACH ROW 
 BEGIN
     INSERT INTO users_audit (
-        audit_id, timestamp, type, creator, username, password, enabled, fk_organisation_id, 
+        audit_id, timestamp, type, db_user, username, password, enabled, fk_organisation_id, 
         first_name, last_name, email, authority_set_date
 	)
     VALUES(
@@ -270,17 +270,241 @@ BEGIN
 END $$ 
 DELIMITER ;
 
+-- =========================================================
+-- Documents Auditing 
+-- =========================================================
 
--- Test values used to test triggers left here for demonstration 
--- INSERT INTO `users` (`username`,`password`, `enabled`, `fk_organisation_id`,`first_name`, `last_name`, `email`, `authority_set_date`)
--- VALUES ('audit_test','$2a$10$9ch3QV3gYNS7lPW/m.TUr.LcH9uEynCbmbGGocRkBAavzRzU0mYa.', 1, 1, 'John','Smith','John@Audit.ac.uk',NOW());
+DROP TABLE IF EXISTS `documents_audit`;
+CREATE TABLE IF NOT EXISTS `documents_audit` (
+    `audit_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `timestamp` DATETIME NOT NULL ,
+    `type` VARCHAR(20) NOT NULL,
+    `db_user` VARCHAR(250) NOT NULL,
+    `fk_creator` VARCHAR(50) NOT NULL,
+    `title` VARCHAR(100) NOT NULL,
+    `date` VARCHAR(20) NOT NULL,
+    `file_type` VARCHAR(20) NOT NULL,
+    `file_data` LONGBLOB
+);
 
--- UPDATE users
--- SET first_name= 'Bill'
--- WHERE username = 'audit_test';
+DROP TRIGGER IF EXISTS documents_insert_audit_trigger;
+DELIMITER $$ 
+CREATE TRIGGER documents_insert_audit_trigger
+AFTER INSERT ON documents FOR EACH ROW 
+BEGIN
+    INSERT INTO documents_audit (
+        audit_id, timestamp, type, db_user, 
+        fk_creator, title, date, file_type, file_data
+	)
+    VALUES(
+		null, CURRENT_TIMESTAMP, 'INSERT', user(), NEW.fk_creator, NEW.title,
+		NEW.date, NEW.file_type, NEW.file_data
+	);
+END $$ 
+DELIMITER ;
 
--- DELETE FROM users WHERE username = 'audit_test';
+DROP TRIGGER IF EXISTS documents_update_audit_trigger;
+DELIMITER $$ 
+CREATE TRIGGER documents_update_audit_trigger
+AFTER UPDATE ON documents FOR EACH ROW 
+BEGIN
+    INSERT INTO documents_audit (
+        audit_id, timestamp, type, db_user, 
+        fk_creator, title, date, file_type, file_data
+	)
+    VALUES(
+		null, CURRENT_TIMESTAMP, 'UPDATE', user(), NEW.fk_creator, NEW.title,
+		NEW.date, NEW.file_type, NEW.file_data
+	);
+END $$ 
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS documents_delete_audit_trigger;
+DELIMITER $$ 
+CREATE TRIGGER documents_delete_audit_trigger
+AFTER DELETE ON documents FOR EACH ROW 
+BEGIN
+    INSERT INTO documents_audit (
+        audit_id, timestamp, type, db_user, 
+        fk_creator, title, date, file_type, file_data
+	)
+    VALUES(
+		null, CURRENT_TIMESTAMP, 'DELETE', user(), OLD.fk_creator, OLD.title,
+		OLD.date, OLD.file_type, OLD.file_data
+	);
+END $$ 
+DELIMITER ;
+
 -- ==============================
 -- End 
 -- ==============================
+
+-- Code to test documents triggers
+-- UPDATE documents
+-- SET title = 'an updated title'
+-- WHERE id = 1;
+
+-- DELETE FROM documents WHERE id = 1;
+
+
+
+-- ------------------------------------
+-- SAMPLE DATA
+-- ------------------------------------
+use `graphium`;
+
+SET FOREIGN_KEY_CHECKS = 0;
+truncate table `document_access_rights`;
+truncate table `users`;
+truncate table `authorities`;
+truncate table `documents`;
+truncate table `organisations`;
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- -----------------------------------
+-- Organisations 
+-- -----------------------------------
+INSERT INTO `organisations` (`id`,`name`,`email`) VALUES (null, 'Cardiff University', 'Cardiff@CardiffEmail.com');
+INSERT INTO `organisations` (`id`,`name`,`email`) VALUES (null, 'Swansea University', 'Swansea@SwanseaEmail.com');
+
+-- ------------------------------------
+-- Users
+-- ------------------------------------
+-- both the passwords decrypt to just be password (using bcrypt)
+-- If usernames are changed, make sure to refactor all tests using them or they will error
+INSERT INTO `users` (`username`,`password`, `enabled`, `fk_organisation_id`,`first_name`, `last_name`, `email`, `authority_set_date`)
+VALUES ('testUser','$2a$10$9ch3QV3gYNS7lPW/m.TUr.LcH9uEynCbmbGGocRkBAavzRzU0mYa.', 1, 1, 'John','Smith','John@Cardiff.ac.uk',NOW());
+
+INSERT INTO `users` (`username`,`password`, `enabled`, `fk_organisation_id`,`first_name`, `last_name`, `email`, `authority_set_date`)
+VALUES ('testUser2','$2a$10$9ch3QV3gYNS7lPW/m.TUr.LcH9uEynCbmbGGocRkBAavzRzU0mYa.', 1, 2, 'John','Smith','John@Swansea.ac.uk',NOW());
+
+INSERT INTO `users` (`username`,`password`, `enabled`, `fk_organisation_id`,`first_name`, `last_name`, `email`, `authority_set_date`)
+VALUES ('testUser3','$2a$10$9ch3QV3gYNS7lPW/m.TUr.LcH9uEynCbmbGGocRkBAavzRzU0mYa.', 1, 1, 'John','Smith','John@Swansea.ac.uk',NOW());
+
+INSERT INTO `users` (`username`,`password`, `enabled`, `fk_organisation_id`,`first_name`, `last_name`, `email`, `authority_set_date`)
+VALUES ('testUser4','$2a$10$9ch3QV3gYNS7lPW/m.TUr.LcH9uEynCbmbGGocRkBAavzRzU0mYa.', 1, 2, 'John','Smith','John@Swansea.ac.uk',NOW());
+
+INSERT INTO `users` (`username`,`password`, `enabled`, `fk_organisation_id`,`first_name`, `last_name`, `email`, `authority_set_date`)
+VALUES ('testOrgAdmin','$2a$10$9ch3QV3gYNS7lPW/m.TUr.LcH9uEynCbmbGGocRkBAavzRzU0mYa.', 1, 1, 'John','Smith','John@Cardiff.ac.uk',NOW());
+
+INSERT INTO `users` (`username`,`password`, `enabled`, `fk_organisation_id`,`first_name`, `last_name`, `email`, `authority_set_date`)
+VALUES ('testOrgAdmin2','$2a$10$9ch3QV3gYNS7lPW/m.TUr.LcH9uEynCbmbGGocRkBAavzRzU0mYa.', 1, 2, 'John','Smith','John@Swansea.ac.uk',NOW());
+
+INSERT INTO `users` (`username`,`password`, `enabled`, `fk_organisation_id`,`first_name`, `last_name`, `email`, `authority_set_date`)
+VALUES ('testSystemAdmin','$2a$10$9ch3QV3gYNS7lPW/m.TUr.LcH9uEynCbmbGGocRkBAavzRzU0mYa.', 1, 1, 'John','Smith','John@Cardiff.ac.uk',NOW());
+
+INSERT INTO `users` (`username`,`password`, `enabled`, `fk_organisation_id`,`first_name`, `last_name`, `email`, `authority_set_date`)
+VALUES ('invalid_username','$2a$10$9ch3QV3gYNS7lPW/m.TUr.LcH9uEynCbmbGGocRkBAavzRzU0mYa.', 1, 1, 'John','Smith','John@Cardiff.ac.uk',NOW());
+
+-- ------------------------
+-- Authorities
+-- ------------------------
+INSERT INTO `authorities` (`fk_username`, `authority`) VALUES ('testUser', 'researcher');
+INSERT INTO `authorities` (`fk_username`, `authority`) VALUES ('testUser2', 'researcher');
+INSERT INTO `authorities` (`fk_username`, `authority`) VALUES ('testUser3', 'researcher');
+INSERT INTO `authorities` (`fk_username`, `authority`) VALUES ('testUser4', 'researcher');
+INSERT INTO `authorities` (`fk_username`, `authority`) VALUES ('testOrgAdmin', 'orgAdmin');
+INSERT INTO `authorities` (`fk_username`, `authority`) VALUES ('testOrgAdmin2', 'orgAdmin');
+INSERT INTO `authorities` (`fk_username`, `authority`) VALUES ('testSystemAdmin', 'systemAdmin');
+
+
+-- --------------------------
+-- Documents
+-- ---------------------------
+INSERT INTO `documents` (`id`, `fk_creator`, `title`, `date`, `file_type`, `file_data`)
+VALUES (null, 'testUser', 'An Excellent File', '2021-10-13' ,'pdf', null);
+
+INSERT INTO `documents` (`id`, `fk_creator`, `title`, `date`, `file_type`, `file_data`)
+VALUES (null, 'testUser', 'A Fun PDF', '2021-12-14' ,'pdf', null);
+
+INSERT INTO `documents` (`id`, `fk_creator`, `title`, `date`, `file_type`, `file_data`)
+VALUES (null, 'testUser', 'Word Test', '2021-11-08' ,'docx', null);
+
+INSERT INTO `documents` (`id`, `fk_creator`, `title`, `date`, `file_type`, `file_data`)
+VALUES (null, 'testUser2', 'Different Org 1', '2021-07-26' ,'pdf', null);
+
+INSERT INTO `documents` (`id`, `fk_creator`, `title`, `date`, `file_type`, `file_data`)
+VALUES (null, 'testUser2', 'Different Org 2', '2021-04-06' ,'pdf', null);
+
+INSERT INTO `documents` (`id`, `fk_creator`, `title`, `date`, `file_type`, `file_data`)
+VALUES (null, 'testUser2', 'Different Org 3', '2020-12-31' ,'pdf', null);
+
+INSERT INTO `documents` (`id`, `fk_creator`, `title`, `date`, `file_type`, `file_data`)
+VALUES (null, 'testUser2', 'Swansea Uni PDF', '2021-12-03' ,'pdf', null);
+
+
+-- --------------------------
+-- Document Access Rights
+-- --------------------------
+INSERT INTO `document_access_rights` (`id`, `fk_document_id`, `fk_organisation_id`, `fk_user_id`)
+VALUES (null, 1, 1, null);
+INSERT INTO `document_access_rights` (`id`, `fk_document_id`, `fk_organisation_id`, `fk_user_id`)
+VALUES (null, 2, null, 'testUser2');
+INSERT INTO `document_access_rights` (`id`, `fk_document_id`, `fk_organisation_id`, `fk_user_id`)
+VALUES (null, 4, 1, null);
+INSERT INTO `document_access_rights` (`id`, `fk_document_id`, `fk_organisation_id`, `fk_user_id`)
+VALUES (null, 5, 1, null);
+
+
+-- --------------------------
+-- Access_Audit_Actions
+-- --------------------------
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'User logged In - Success');
+
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'User logged In - Failed');
+
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'All system documents accessed - Success');
+
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'All system documents accessed - Failed');
+
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'Organisation documents accessed - Success');
+
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'Organisation documents accessed - Failed');
+
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'Document downloaded');
+
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'Document deleted');
+
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'Document viewed');
+
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'New organisation created');
+
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'New user created');
+
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'File uploaded');
+
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'File upload failed');
+
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'Document download failed');
+
+INSERT INTO `access_audit_actions` (`id`, `action_description`)
+VALUES (null, 'View document failed');
+
+
+-- --------------------------
+-- AccessAuditReports
+-- --------------------------
+INSERT INTO `access_audit_reports` (`id`, `fk_username`, `fk_document_id`,`fk_organisation_id`, `fk_action_id`, `action_date`, `action_description`)
+VALUES (null, 'testUser', null, null,1, NOW(), 'User Logged in (Test Data)');
+
+INSERT INTO `access_audit_reports` (`id`, `fk_username`, `fk_document_id`,`fk_organisation_id`, `fk_action_id`, `action_date`, `action_description`)
+VALUES (null, 'testUser', null, 1,6, NOW(), 'Organisation Documents Accessed Failed Attempt (Test Data)');
+
+INSERT INTO `access_audit_reports` (`id`, `fk_username`, `fk_document_id`,`fk_organisation_id`, `fk_action_id`, `action_date`, `action_description`)
+VALUES (null, 'testUser', 1, null,7, NOW(), 'Document Downloaded (Test Data)');
+
 
