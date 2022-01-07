3a7727d0d1cc5afed1122233ed3f2156e9abf5dc
Archie Wilkins <wilkinsat1@cardiff.ac.uk>
2022-01-07 21:16:13 +0000

    All tables are now audited

--- src/main/resources/schema-maria.sql
+++ src/main/resources/schema-maria.sql
@@ -11,65 +11,65 @@
 -- ====================================
 
 -- Senior Developer (Trusted with more privilidges such as drop table) 
-CREATE USER 'MarkPaulSuperAdmin'@'127.0.0.1' IDENTIFIED BY 'PlantBridgeTrain';
+-- CREATE USER 'MarkPaulSuperAdmin'@'127.0.0.1' IDENTIFIED BY 'PlantBridgeTrain';
 
-GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, CREATE, DROP, GRANT OPTION ON graphium.documents TO 'MarkPaulSuperAdmin'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, CREATE, DROP, GRANT OPTION ON graphium.document_access_rights TO 'MarkPaulSuperAdmin'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, CREATE, DROP, GRANT OPTION ON graphium.users TO 'MarkPaulSuperAdmin'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, CREATE, DROP, GRANT OPTION ON graphium.authorities TO 'MarkPaulSuperAdmin'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, CREATE, DROP, GRANT OPTION ON graphium.organisations TO 'MarkPaulSuperAdmin'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, CREATE, DROP, GRANT OPTION ON graphium.access_audit_actions TO 'MarkPaulSuperAdmin'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, CREATE, DROP, GRANT OPTION ON access_audit_reports TO 'MarkPaulSuperAdmin'@'127.0.0.1';
-GRANT SELECT, ALTER, CREATE, GRANT OPTION ON graphium.documents_audit TO 'MarkPaulSuperAdmin'@'127.0.0.1';
-GRANT SELECT, ALTER, CREATE, GRANT OPTION ON graphium.users_audit TO 'MarkPaulSuperAdmin'@'127.0.0.1';
-
-
-CREATE USER 'KevinWilliamsSeniorDeveloper'@'127.0.0.1' IDENTIFIED BY 'HorseLightPlane';
-
-GRANT SELECT, UPDATE, DELETE, INSERT, ALTER ON graphium.documents TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, ALTER ON graphium.document_access_rights TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, ALTER ON graphium.users TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, ALTER ON graphium.authorities TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, ALTER ON graphium.organisations TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, ALTER ON graphium.access_audit_actions TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, ALTER ON access_audit_reports TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
-GRANT SELECT ON graphium.documents_audit TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
-GRANT SELECT ON graphium.users_audit TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, CREATE, DROP, GRANT OPTION ON graphium.documents TO 'MarkPaulSuperAdmin'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, CREATE, DROP, GRANT OPTION ON graphium.document_access_rights TO 'MarkPaulSuperAdmin'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, CREATE, DROP, GRANT OPTION ON graphium.users TO 'MarkPaulSuperAdmin'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, CREATE, DROP, GRANT OPTION ON graphium.authorities TO 'MarkPaulSuperAdmin'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, CREATE, DROP, GRANT OPTION ON graphium.organisations TO 'MarkPaulSuperAdmin'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, CREATE, DROP, GRANT OPTION ON graphium.access_audit_actions TO 'MarkPaulSuperAdmin'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, CREATE, DROP, GRANT OPTION ON access_audit_reports TO 'MarkPaulSuperAdmin'@'127.0.0.1';
+-- GRANT SELECT, ALTER, CREATE, GRANT OPTION ON graphium.documents_audit TO 'MarkPaulSuperAdmin'@'127.0.0.1';
+-- GRANT SELECT, ALTER, CREATE, GRANT OPTION ON graphium.users_audit TO 'MarkPaulSuperAdmin'@'127.0.0.1';
 
 
+-- CREATE USER 'KevinWilliamsSeniorDeveloper'@'127.0.0.1' IDENTIFIED BY 'HorseLightPlane';
 
--- -- Junior Developer (Given less priviledges)
-CREATE USER 'ChrisHansenJuniorDeveloper'@'127.0.0.1' IDENTIFIED BY 'DogSpeakerMarmalade';
-
-GRANT SELECT, UPDATE, DELETE ON graphium.documents TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE ON graphium.document_access_rights TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT ON graphium.users TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT ON graphium.authorities TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE ON graphium.organisations TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
-GRANT SELECT ON graphium.access_audit_actions TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
-GRANT SELECT ON graphium.access_audit_reports TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
-GRANT SELECT ON graphium.documents_audit TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
-GRANT SELECT ON graphium.users_audit TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, INDEX ON graphium.documents TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, INDEX ON graphium.document_access_rights TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, INDEX ON graphium.users TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, INDEX ON graphium.authorities TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, INDEX ON graphium.organisations TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, INDEX ON graphium.access_audit_actions TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, ALTER, INDEX ON access_audit_reports TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT ON graphium.documents_audit TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT ON graphium.users_audit TO 'KevinWilliamsSeniorDeveloper'@'127.0.0.1';
 
 
--- -- Mark Strange represents a user who should only be able to view database data for example he may be a 
--- -- customer support assistant who should be able to look up a users account, documents, users actions without updating them
--- -- the decisions to not allow MarkStrange to update,delete or insert data was on the basis that Sam may not have developer expierence 
--- -- so should not have access to potentially disruptive or destructive commands.
+
+-- -- -- Junior Developer (Given less priviledges)
+-- CREATE USER 'ChrisHansenJuniorDeveloper'@'127.0.0.1' IDENTIFIED BY 'DogSpeakerMarmalade';
+
+-- GRANT SELECT, UPDATE, DELETE ON graphium.documents TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE ON graphium.document_access_rights TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT ON graphium.users TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE, INSERT ON graphium.authorities TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT, UPDATE, DELETE ON graphium.organisations TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT ON graphium.access_audit_actions TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT ON graphium.access_audit_reports TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT ON graphium.documents_audit TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
+-- GRANT SELECT ON graphium.users_audit TO 'ChrisHansenJuniorDeveloper'@'127.0.0.1';
+
+
+-- Mark Strange represents a user who should only be able to view database data for example he may be a 
+-- customer support assistant who should be able to look up a users account, documents, users actions without updating them
+-- the decisions to not allow MarkStrange to update,delete or insert data was on the basis that Sam may not have developer expierence 
+-- so should not have access to potentially disruptive or destructive commands.
 -- CREATE USER 'MarkStrangeMarketingExpert'@'127.0.0.1' IDENTIFIED BY 'HorseLightPlane';
 -- GRANT SELECT ON graphium.users TO 'MarkStrangeMarketingExpert'@'127.0.0.1';
 -- GRANT SELECT ON graphium.documents TO 'MarkStrangeMarketingExpert'@'127.0.0.1';
 
--- System 
-CREATE USER 'GraphiumApplication'@'127.0.0.1' IDENTIFIED BY 'PrincePandaDepot';
+-- -- System 
+-- CREATE USER 'GraphiumApplication'@'localhost' IDENTIFIED BY 'PrincePandaDepot';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, CREATE, DROP, INDEX ON graphium.documents TO 'GraphiumApplication'@'localhost';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, CREATE, DROP, INDEX ON graphium.document_access_rights TO 'GraphiumApplication'@'localhost';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, CREATE, DROP, INDEX ON graphium.users TO 'GraphiumApplication'@'localhost';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, CREATE, DROP, INDEX ON graphium.authorities TO 'GraphiumApplication'@'localhost';
+-- GRANT SELECT, UPDATE, DELETE, INSERT, CREATE, DROP, INDEX ON graphium.organisations TO 'GraphiumApplication'@'localhost';
+-- GRANT INSERT, CREATE, DROP ON graphium.access_audit_actions TO 'GraphiumApplication'@'localhost';
+-- GRANT INSERT, CREATE, DROP ON graphium.access_audit_reports TO 'GraphiumApplication'@'localhost';
 
-GRANT SELECT, UPDATE, DELETE, INSERT, CREATE, DROP ON graphium.documents TO 'GraphiumApplication'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, CREATE, DROP ON graphium.document_access_rights TO 'GraphiumApplication'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, CREATE, DROP ON graphium.users TO 'GraphiumApplication'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, CREATE, DROP ON graphium.authorities TO 'GraphiumApplication'@'127.0.0.1';
-GRANT SELECT, UPDATE, DELETE, INSERT, CREATE, DROP ON graphium.organisations TO 'GraphiumApplication'@'127.0.0.1';
-GRANT INSERT, CREATE, DROP ON graphium.access_audit_actions TO 'GraphiumApplication'@'127.0.0.1';
-GRANT INSERT, CREATE, DROP ON graphium.access_audit_reports TO 'GraphiumApplication'@'127.0.0.1';
 
 -- ==============================
 -- End of section to comment out 
@@ -312,7 +312,7 @@ CREATE UNIQUE INDEX `ix_auth_username`
 --     `date` VARCHAR(20) NOT NULL,
 --     `file_type` VARCHAR(20) NOT NULL,
 --     `file_data` LONGBLOB
--- );
+-- ); 
 
 -- DROP TRIGGER IF EXISTS documents_insert_audit_trigger;
 -- DELIMITER $$ 
@@ -362,6 +362,193 @@ CREATE UNIQUE INDEX `ix_auth_username`
 -- END $$ 
 -- DELIMITER ;
 
+-- -- =========================================================
+-- -- Organisations Auditing 
+-- -- =========================================================
+
+-- DROP TABLE IF EXISTS `organisations_audit`;
+-- CREATE TABLE IF NOT EXISTS `organisations_audit` (
+--     `audit_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     `timestamp` DATETIME NOT NULL ,
+--     `type` VARCHAR(20) NOT NULL,
+--     `db_user` VARCHAR(250) NOT NULL,
+--     `org_id` int,
+--     `name` VARCHAR(100) NOT NULL,
+--     `email` VARCHAR(100)
+--     
+-- );
+
+-- DROP TRIGGER IF EXISTS organisations_insert_audit_trigger;
+-- DELIMITER $$ 
+-- CREATE TRIGGER organisations_insert_audit_trigger
+-- AFTER INSERT ON organisations FOR EACH ROW 
+-- BEGIN
+--     INSERT INTO organisations_audit (
+--         audit_id, timestamp, type, db_user, 
+--         org_id, name, email
+-- 	)
+--     VALUES(
+-- 		null, CURRENT_TIMESTAMP, 'INSERT', user(), NEW.id, NEW.name,
+-- 		NEW.email
+-- 	);
+-- END $$ 
+-- DELIMITER ;
+
+-- DROP TRIGGER IF EXISTS organisations_update_audit_trigger;
+-- DELIMITER $$ 
+-- CREATE TRIGGER organisations_update_audit_trigger
+-- AFTER UPDATE ON organisations FOR EACH ROW 
+-- BEGIN
+--     INSERT INTO organisations_audit (
+--        audit_id, timestamp, type, db_user, 
+--         org_id, name, email
+-- 	)
+--     VALUES(
+-- 		null, CURRENT_TIMESTAMP, 'UPDATE', user(), NEW.id, NEW.name,
+-- 		NEW.email
+-- 	);
+-- END $$ 
+-- DELIMITER ;
+
+-- DROP TRIGGER IF EXISTS organisations_delete_audit_trigger;
+-- DELIMITER $$ 
+-- CREATE TRIGGER organisations_delete_audit_trigger
+-- AFTER DELETE ON organisations FOR EACH ROW 
+-- BEGIN
+--     INSERT INTO organisations_audit (
+--         audit_id, timestamp, type, db_user, 
+--         org_id, name, email
+-- 	)
+--     VALUES(
+-- 		null, CURRENT_TIMESTAMP, 'DELETE', user(), OLD.id, OLD.name,
+-- 		OLD.email
+-- 	);
+-- END $$ 
+-- DELIMITER ;
+
+-- =============================
+-- Authorities
+-- =============================
+
+-- DROP TABLE IF EXISTS `authorities_audit`;
+-- CREATE TABLE IF NOT EXISTS `authorities_audit` (
+--     `audit_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     `timestamp` DATETIME NOT NULL ,
+--     `type` VARCHAR(20) NOT NULL,
+--     `db_user` VARCHAR(250) NOT NULL,
+--     `fk_username` VARCHAR(50) NOT NULL,
+--     `authority` VARCHAR(50) NOT NULL
+-- );
+
+-- DROP TRIGGER IF EXISTS authorities_insert_audit_trigger;
+-- DELIMITER $$ 
+-- CREATE TRIGGER authorities_insert_audit_trigger
+-- AFTER INSERT ON authorities FOR EACH ROW 
+-- BEGIN
+--     INSERT INTO authorities_audit (
+--         audit_id, timestamp, type, db_user, 
+--         fk_username, authority
+-- 	)
+--     VALUES(
+-- 		null, CURRENT_TIMESTAMP, 'INSERT', user(), NEW.fk_username, NEW.authority
+-- 	);
+-- END $$ 
+-- DELIMITER ;
+
+-- DROP TRIGGER IF EXISTS authorities_update_audit_trigger;
+-- DELIMITER $$ 
+-- CREATE TRIGGER authorities_update_audit_trigger
+-- AFTER UPDATE ON authorities FOR EACH ROW 
+-- BEGIN
+--     INSERT INTO authorities_audit (
+--        audit_id, timestamp, type, db_user, 
+--         fk_username, authority
+-- 	)
+--     VALUES(
+-- 		null, CURRENT_TIMESTAMP, 'UPDATE', user(), NEW.fk_username, NEW.authority
+-- 	);
+-- END $$ 
+-- DELIMITER ;
+
+-- DROP TRIGGER IF EXISTS authorities_delete_audit_trigger;
+-- DELIMITER $$ 
+-- CREATE TRIGGER authorities_delete_audit_trigger
+-- AFTER DELETE ON authorities FOR EACH ROW 
+-- BEGIN
+--     INSERT INTO authorities_audit (
+--         audit_id, timestamp, type, db_user, 
+--         fk_username, authority
+-- 	)
+--     VALUES(
+-- 		null, CURRENT_TIMESTAMP, 'DELETE', user(), OLD.fk_username, OLD.authority
+-- 	);
+-- END $$ 
+-- DELIMITER ;
+
+
+-- =============================
+-- Document_Access_Rights
+-- =============================
+
+
+-- DROP TABLE IF EXISTS `document_access_rights_audit`;
+-- CREATE TABLE IF NOT EXISTS `document_access_rights_audit` (
+--     `audit_id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     `timestamp` DATETIME NOT NULL ,
+--     `type` VARCHAR(20) NOT NULL,
+--     `db_user` VARCHAR(250) NOT NULL,
+-- 	`right_id` INT(5),
+--     `fk_document_id` INT(4) NOT NULL,
+--     `fk_organisation_id` INT NULL,
+--     `fk_user_id` VARCHAR(50) NULL
+-- );
+
+-- DROP TRIGGER IF EXISTS document_access_rights_insert_audit_trigger;
+-- DELIMITER $$ 
+-- CREATE TRIGGER document_access_rights_insert_audit_trigger
+-- AFTER INSERT ON document_access_rights FOR EACH ROW 
+-- BEGIN
+--     INSERT INTO document_access_rights_audit (
+--         audit_id, timestamp, type, db_user, 
+--         right_id, fk_document_id, fk_organisation_id, fk_user_id
+-- 	)
+--     VALUES(
+-- 		null, CURRENT_TIMESTAMP, 'INSERT', user(), NEW.id, NEW.fk_document_id, NEW.fk_organisation_id, NEW.fk_user_id
+-- 	);
+-- END $$ 
+-- DELIMITER ;
+
+-- DROP TRIGGER IF EXISTS document_access_rights_update_audit_trigger;
+-- DELIMITER $$ 
+-- CREATE TRIGGER document_access_rights_update_audit_trigger
+-- AFTER UPDATE ON document_access_rights FOR EACH ROW 
+-- BEGIN
+--     INSERT INTO document_access_rights_audit (
+--        audit_id, timestamp, type, db_user, 
+--         right_id, fk_document_id, fk_organisation_id, fk_user_id
+-- 	)
+--     VALUES(
+-- 		null, CURRENT_TIMESTAMP, 'UPDATE', user(), NEW.id, NEW.fk_document_id, NEW.fk_organisation_id, NEW.fk_user_id
+-- 	);
+-- END $$ 
+-- DELIMITER ;
+
+-- DROP TRIGGER IF EXISTS document_access_rights_delete_audit_trigger;
+-- DELIMITER $$ 
+-- CREATE TRIGGER document_access_rights_delete_audit_trigger
+-- AFTER DELETE ON document_access_rights FOR EACH ROW 
+-- BEGIN
+--     INSERT INTO document_access_rights_audit (
+--         audit_id, timestamp, type, db_user, 
+--         right_id, fk_document_id, fk_organisation_id, fk_user_id
+-- 	)
+--     VALUES(
+-- 		null, CURRENT_TIMESTAMP, 'DELETE', user(), OLD.id, OLD.fk_document_id, OLD.fk_organisation_id, OLD.fk_user_id
+-- 	);
+-- END $$ 
+-- DELIMITER ;
+
+
 -- ==============================
 -- End 
 -- ==============================
@@ -372,6 +559,7 @@ CREATE UNIQUE INDEX `ix_auth_username`
 -- WHERE id = 1;
 
 -- DELETE FROM documents WHERE id = 1;
+
 
 
 
